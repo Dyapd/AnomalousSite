115168118fa64ab8b059955c2e682b9c0789cb64
Allen Payad <allenpayad1@gmail.com>
2024-01-11 09:40:56 +0800

    added history page

--- database/entrySubmission.php
+++ database/entrySubmission.php
@@ -1,4 +1,35 @@
  <?php
+ 
+
+
+  $con = new mysqli("localhost", "root", "", "sdpdatabase");
+  if($con->connect_error) 
+  {
+    die("Failed to connect : ".$con->connect_error);
+  }
+  else 
+  {
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+    $title = $_POST['title'];
+    $report = $_POST['report'];
+    $type = $_POST['type']; 
+    $threat = $_POST['threat'];
+    $status = $_POST['status'];
+
+    $image = $_FILES["img"]['name']
+
+    $stmt = $con->prepare("INSERT INTO entrysubdb(name, location, title, report, type, threat, status) values (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssss", $name, $location, $title, $report, $type, $threat, $status);
+    $stmt->execute();
+    
+    $stmt->close();
+    $con->close();
+  }
+
+?> 
+
+<!-- <?php
   $name = $_POST['name'];
   $location = $_POST['location'];
   $title = $_POST['title'];
@@ -23,4 +54,5 @@
     $con->close();
   }
 
-?> 
+?>  -->
+
